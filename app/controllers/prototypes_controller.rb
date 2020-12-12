c2613992ab32df31acd31c0906494895dbc36522
sugastreet22 <re22sugar@gmail.com>
2020-12-12 12:33:21 +0900

    投稿機能を実装しましょう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,23 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def index
   end
 
   def new
+    @prototype = Prototype.new
   end
 
   def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
