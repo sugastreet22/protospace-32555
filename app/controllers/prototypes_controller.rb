f72d0cd265f4431c33d955905897b27298504651
sugastreet22 <re22sugar@gmail.com>
2020-12-16 19:07:34 +0900

    投稿したコメントが詳細ページで表示されるようにしましょう

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -20,6 +20,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
   end
 
   def edit
